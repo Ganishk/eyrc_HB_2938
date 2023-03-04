0074f89e5533e11fb8a30231d5a85f4e631ebe7c
Ganishk <ganishk1@gmail.com>
2023-03-04 20:12:14 +0530

    finalized code

--- Task 5A/controller.py
+++ Task 5A/controller.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python3
 
+'''
+*****************************************************************************************
+*
+*        		===============================================
+*           		    HolA Bot (HB) Theme (eYRC 2022-23)
+*        		===============================================
+*
+*  This script should be used to implement Task 0 of HolA Bot (HB) Theme (eYRC 2022-23).
+*
+*  This software is made available on an "AS IS WHERE IS BASIS".
+*  Licensee/end user indemnifies and will keep e-Yantra indemnified from
+*  any and all claim(s) that emanate from the use of the Software or
+*  breach of the terms of this agreement.
+*
+*****************************************************************************************
+'''
+
+# Team ID:  HB#2938	[ Team-ID ]
+# Author List: Ganishk D, Srivattsan S, Susmitha S, Janam Khandhelwal	[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
+# Filename:		controller.py
+# Functions:
+#			[ Comma separated list of functions in this file ]
+# Nodes:		Add your publishing and subscribing node
+
+################################################################################ 
+################################### MODULES ####################################
+################################################################################
 import cv2 as cv
 from cv2 import aruco
 import numpy as np
@@ -18,19 +45,20 @@ from cv_bridge import CvBridge
 
 global conn,s
 
-IP      = "127.0.0.1"
+IP      = "192.168.1.123"
 PORT    = 8002
 PI      = 3.141592653589793
-dTOL    = 5
-aTOL    = 1
-Kpd     = 20
+dTOL    = 3
+aTOL    = 0.05
+Kpd     = 220
+KpA     = 1000
 msg     = ""
 flag    = True
 
-x_goals = [350,150,150,350]
-y_goals = [300,300,150,150]
-theta_goals = [0.25*PI,0.75*PI,-0.75*PI,-0.25*PI]
-#x,y,theta = (0,0,0)
+x_goals = [350,150,150,350,]
+y_goals = [300,300,150,150,]
+theta_goals = [0.25*PI,0.75*PI,-0.75*PI,-0.25*PI,]
+x,y,theta = (0,0,0)
 
 ################################################################################ 
 ################################## FUNCTIONS ################################### 
@@ -46,12 +74,6 @@ def cleanup():
     cv.destroyAllWindows()
     print("cleanup done")
 
-def goals():
-    pass
-
-def callback():
-    pass
-
 ################################################################################ 
 ############################## INVERSE KINEMATICS ##############################
 ################################################################################
@@ -61,7 +83,9 @@ def inverse_kinematics(Vx,Vy,W):
     v1 = Vx - W
     v2 = -0.5*Vx - 0.866*Vy - W
     v3 = -0.5*Vx + 0.866*Vy - W
-    msg = f"{v1} {v2} {v3}"
+    # Using less decimal places to send less data, which increases transmission
+    # speed
+    msg = f"{v1:.3f} {v2:.3f} {v3:.3f}\n"
     teleport()
 
 ################################################################################ 
@@ -69,7 +93,6 @@ def inverse_kinematics(Vx,Vy,W):
 ################################################################################
 
 def p():
-    global theta,x_goal,y_goal,theta_goal,x,y,theta
     while True:
         e_x = x_goal - x
         e_y = y_goal - y
@@ -84,15 +107,17 @@ def p():
         else: Kpy = Kpd
 
         if abs(e_theta) <= aTOL: Kpa = 0
-        else: Kpa = 30
+        else: Kpa = KpA
         
         Vx = Kpx*E_x
         Vy = Kpy*E_y
         W = Kpa*e_theta
 
         inverse_kinematics(Vx,Vy,W)
+        print(Vx,Vy,W)
 
         if not(abs(E_x) > dTOL or abs(E_y) > dTOL or abs(e_theta) > aTOL):
+            print("Condition satisfied:",x,y,theta)
             sleep(1)
             break
 
@@ -101,9 +126,8 @@ def p():
 ################################################################################
 
 def teleport():
-    global conn,msg
-    print(msg)
     conn.sendall(msg.encode())
+    conn.recv(1)
 
 def establish():
     global conn,s
@@ -114,7 +138,7 @@ def establish():
     s.bind((IP,PORT))
     print("Binded successfully.")
     s.listen()
-    conn, addr = s.accept()
+    conn,addr = s.accept()
 
     print(f"Connected by {addr} at {IP}:{PORT}")
 
@@ -157,7 +181,6 @@ def perspection(frame):
 def getPose(data):
     global x,y,theta,matrix
 
-    #rospy.loginfo("Locating bot")
     br = CvBridge()
     frame = br.imgmsg_to_cv2(data,"mono8")
     #frame = cv.imread("pose2.png")
@@ -174,17 +197,15 @@ def getPose(data):
                 (tL,tR,bR,bL) = corner[0]
                 x = (tL[0] + tR[0] + bR[0] + bL[0])/4 #-#
                 y = 500 - (tL[1] + tR[1] + bR[1] + bL[1])/4 #-#
-                #tC =((tL[0]+tR[0])/2, 500 - (tL[1]+tR[1])/2 )
                 rC =((bR[0]+tR[0])/2, 500 - (bR[1]+tR[1])/2 )
                 dX = rC[0] - x
                 dY = rC[1] - y
                 theta = math.atan2(dY,dX) #-#
-                #y = 500 - y #-#
-                print(f"Found the bot at x={x},y={y},\u03b8={theta}", x,y,theta)
+                #print(f"Found the bot at x={x},y={y},\u03b8={theta}")
 
-    cv.putText(result,f"x: {x}",(30,40),cv.FONT_HERSHEY_SIMPLEX,0.5,(0,),2)
-    cv.putText(result,f"y: {y}",(30,60),cv.FONT_HERSHEY_SIMPLEX,0.5,0xff,2)
-    cv.putText(result,f"t: {theta:.2f}",(30,80),cv.FONT_HERSHEY_SIMPLEX,0.5,(0xff,),1)
+        cv.putText(result,f"x: {x}",(30,40),cv.FONT_HERSHEY_SIMPLEX,0.5,0xff,1)
+        cv.putText(result,f"y: {y}",(30,60),cv.FONT_HERSHEY_SIMPLEX,0.5,0xff,1)
+        cv.putText(result,f"t: {theta:.2f}",(30,80),cv.FONT_HERSHEY_SIMPLEX,0.5,0xff,1)
     cv.imshow('arena',result)
     key=cv.waitKey(1)
 
@@ -192,22 +213,27 @@ def getPose(data):
 ##################################### MAIN #####################################
 ################################################################################
 
-def main():
-    global x_goal,y_goal,theta_goal,x,y,theta
-    establish()
-    rospy.init_node('aruco_feedback_node')
-    rospy.Subscriber('usb_cam/image_rect',Image,getPose)
-
-    # wait until the 1st feedback
-    x = 0
-    while not x: print(".",end="")
-    else: print("Starting from",x,y,theta)
-
+def interpolate():
+    global x_goal,y_goal,theta_goal
     while (not rospy.is_shutdown()) and x_goals:
         x_goal = x_goals.pop(0)
         y_goal = y_goals.pop(0)
         theta_goal = theta_goals.pop(0)
         p()
+
+def main():
+    global x,y,theta
+    establish()
+    rospy.init_node('aruco_feedback_node')
+    rospy.Subscriber('usb_cam/image_rect',Image,getPose)
+
+    # wait until the 1st feedback
+    while not x:
+        sleep(.2)
+    else:
+        print("\nStarting from",x,y,theta)
+        input("Press Enter to start the bot.")
+    interpolate()
 
 if __name__=="__main__":
     signal.signal(signal.SIGINT, signal_handler)
--- Task 5A/espSection/espSection.ino
+++ Task 5A/espSection/espSection.ino
@@ -1,72 +1,78 @@
+/**
+ * We had used ESP 32 ( Node MCU 32s ) module so it might be different from the origial eYFi ESP code
+ */
+
 #include <WiFi.h>
 
 // WiFi credentials
-const char* ssid = "Alola";                    //Enter your wifi hotspot ssid
-const char* password =  "theriyaathu";               //Enter your wifi hotspot password
+const char* ssid = "RMI";                    //Enter your wifi hotspot ssid
+const char* password =  "HAL9000T800";               //Enter your wifi hotspot password
 const uint16_t port = 8002;
-const char * host = "192.168.4.2";                   //Enter the ip address of your laptop after connecting it to wifi hotspot
+const char * host = "192.168.1.123";                   //Enter the ip address of your laptop after connecting it to wifi hotspot
 String msg = "0";
 
 
 WiFiClient client;
 
+/**
+ * AVR Presets
+ */
 void avr_section(String);
 
-void setup(){   
-  Serial.begin(9600);                          //Serial to print data on Serial Monitor
-//  Serial1.begin(115200,SERIAL_8N1,33,32);        //Serial to transfer data between ESP and AVR. The Serial connection is inbuilt.
-  
-  
-  Serial.println("Setting AP ...");
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(ssid,password);
-
-  Serial.println();
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP Address: ");
-  Serial.println(IP);
-  xTaskCreatePinnedToCore(loop2,"loop2",1000,NULL,0,NULL,0);
-}
-
-
-void loop() {
-  if (!client.connect(host, port)) {
-    Serial.println("Connection to host failed");
-    delay(200);
-    return;
-  }
-  while(1){
-      msg = client.readStringUntil('\n');
-      msg.trim();
-      //if (msg.length()) avr_section(msg);
-  }
-}
-
-/**
- * 
- * AVR section codes
- *  
- */
 
 #include "AccelStepper.h"
+
+const int MAXSPEED=2500;
 
 AccelStepper stepper1(AccelStepper::DRIVER, 19,18);
 AccelStepper stepper2(AccelStepper::DRIVER, 5,17);
 AccelStepper stepper3(AccelStepper::DRIVER, 16,4);
 float v1,v2,v3;
 
+/**
+ * Common setups
+ */
 
+void setup(){
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid,password);
+
+//  WiFi.softAP(ssid,password);
+//  xTaskCreatePinnedToCore(loop2,"loop2",1000,NULL,0,NULL,0);
+
+  stepper1.setMaxSpeed(MAXSPEED);
+  stepper2.setMaxSpeed(MAXSPEED);
+  stepper3.setMaxSpeed(MAXSPEED);
+}
+
+
+void loop() {
+  if (!client.connect(host, port)) {
+    delay(200);
+    return;
+  }
+  while(1){
+      msg = client.readStringUntil('\n');
+      client.print(" ");
+      //msg.trim();
+      if (msg.length()) avr_section(msg);
+  }
+}
+
+
+/**
+ * 
+ * AVR section codes
+ *  
+ */
 void fine(){
   // Send the signal to drive the motor.
   stepper1.setSpeed(v1);
   stepper2.setSpeed(v2);
   stepper3.setSpeed(v3);
-  unsigned long prev_timer = millis();
-  while(millis() - prev_timer < 3000){
-    stepper1.runSpeed();
-    stepper2.runSpeed();
-    stepper3.runSpeed();
-  }
+  stepper1.runSpeed();
+  stepper2.runSpeed();
+  stepper3.runSpeed();
 }
 
 void avr_section(String msg){
@@ -75,17 +81,5 @@ void avr_section(String msg){
    */
   sscanf(msg.c_str(),"%f %f %f",&v1,&v2,&v3);
   fine();
-//  Serial.print(v1);
-//  Serial.print(" ");
-//  Serial.print(v2);
-//  Serial.print(" ");
-//  Serial.println(v3);
-}
-//bool flag = false;
-void loop2(void* pvParameters){
-//  if (flag=~flag){
-//    pinMode(2,(flag)?HIGH:LOW);
-//    sleep(1000);
-//  }
-  if (msg.length()) avr_section(msg);
+
 }
